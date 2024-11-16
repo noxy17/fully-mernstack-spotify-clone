90ec82633faad80f0130e5878b9a2c8439f568a9
noxy17 <ajayjalal2005@gmail.com>
2024-11-16 23:41:11 +0530

    incomplete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { buttonVariants } from "./components/ui/button";
 function App() {
 
   return (
     <>
-    <h1>hello </h1>
+    <header>
+      <SignedOut>
+        <SignInButton/>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
     </>
-  )
+  );
 }
 
 export default App
